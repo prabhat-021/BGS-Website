c2735d8d558bb51b9d08641e02f24cd43f7a63cf
Prabhat Sehrawat <prabhatsahrawat010203@gmail.com>
2025-03-20 14:57:08 +0530

    new setup

---
+++ src/app/page.js
@@ -0,0 +1,27 @@
+import { CenterHeading } from "@/components/addonFunctions/Start-heading";
+import Expanding from "@/components/homePage/expanding";
+import Features from "@/components/homePage/homeMidSection2/Features";
+import JoinUsCard from "@/components/homePage/homeMidSection2/Features/joinus";
+import Overview from "@/components/homePage/homeMidSection2/Overview";
+import Services from "@/components/homePage/homeMidSection2/services";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Section 1 Home Page */}
+      <div id="section1" className=" min-h-screen bg-gray-200 ">
+        <div className="md:flex justify-center mb-2 ">
+          <Features />
+          <JoinUsCard />
+        </div>
+        <Services />
+        <Expanding />
+      </div>
+      {/* Section-2 About Page */}
+      <div id="section2" className="bg-gray-200 min-h-screen md:px-10 px-2">
+        <CenterHeading heading="About Us" />
+        <Overview />
+      </div>
+    </div>
+  );
+}
